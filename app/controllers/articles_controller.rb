877e637676dfbc73e2744b54d17c945bb341e34b
Mark Struzinski <mark.struzinski@lowes.com>
2017-06-20 23:33:00 -0400

    Added admin user functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,7 +8,7 @@ class ArticlesController < ApplicationController
   end
 
   def index
-    @articles = Article.paginate(page: params[:page], per_page: 2)
+    @articles = Article.paginate(page: params[:page], per_page: 5)
   end
 
   def create
@@ -56,7 +56,7 @@ class ArticlesController < ApplicationController
   end
 
   def require_same_user
-    if current_user != @article.user
+    if current_user != @article.user and !current_user.admin?
       flash[:danger] = "You can only edit or delete your own article"
       redirect_to root_path
     end
